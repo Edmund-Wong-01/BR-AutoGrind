0716b8c6b884fcec52762ff401fb5542801bb148
Edmund <110015817+Edmund-Wong-01@users.noreply.github.com>
2024-12-27 21:55:19 +0800

    Update config.py

--- code/config.py
+++ code/config.py
@@ -12,14 +12,16 @@ ThrottleRedPos = (x5, y5)
 AWSWarningPos = (x6, y6)
 
 # Screenshot portions
+# Start at top left corner, adjust width and height for screenshot size
 MainRoutePortion = (x_start, y_start, width, height)
 SecondaryRoutePortion = (x_start2, y_start2, width2, height2)
 DistancePortion = (x_start3, y_start3, width3, height3)
 CurrentSpeedPortion = (x_start4, y_start4, width4, height4)
 SpeedLimitPortion = (x_start5, y_start5, width5, height5)
 
-# Control settings
-AccelRate = 1.0  # m/s²
-DecelRate = 1.0  # m/s²
-TrainLength = 100.0  # meters
-TrainTopSpeed = 100.0  # kph
+# Train specifications
+# Find at BR Wiki for that train (currently set to class 800)
+AccelRate = 0.7 # m/s²
+DecelRate = 1.2 # m/s²
+TrainLength = 132.0  # meters
+TrainTopSpeed = 201 # kph
